4c58ebbf5e93f38f4211212b5d5f4bf11d219d76
bniepce <brad.niepce@kaptios.com>
2025-01-22 08:27:16 +0100

    Add cookiecutter config

--- src/loggers/wandb.py
+++ src/loggers/wandb.py
@@ -6,6 +6,6 @@ class WnBLogger(object):
         now = datetime.now()
         date_time = now.strftime("%Y_%m_%d_%H_%M_%S_%f")
         wandb.init(project=project_name, name=experiment_name, config=config, id=date_time)
-
+    
     def watch(self, model):
         wandb.watch(model)
